0e0d558421c42007cdbcd2617e3ef0c244805e5d
Guido Favara <guido.favara05@gmail.com>
2020-12-18 18:04:31 -0300

    Master

---
+++ README.md
@@ -0,0 +1,10 @@
+# Netflix
+
+Im Working on this, not ready yet
+
+Estoy desarrollandolo, todavia no esta listo. 
+
+Firebase
+React
+WebPack
+
---
+++ src/app.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { FooterContainer } from './containers/footer';
+import { JumbotronContainer } from './containers/jumbotron';
+
+export default function App() {
+  return (
+    <>
+       <JumbotronContainer />
+       <FooterContainer />
+    </>
+  );
+}
