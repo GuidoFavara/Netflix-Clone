49d2b354d0a3be70d3b8146e0682654eee6fde23
Guido Favara <guido.favara05@gmail.com>
2020-12-18 22:27:51 -0300

    Faqs y Jumbotron update

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 Im Working on this, not ready yet
 
+One of my biggest proyect so far
+
 Estoy desarrollandolo, todavia no esta listo. 
 
-Firebase
+Firebase & auth
 React
 WebPack
 
+1° Step Scraping Netflix Original Web to get al elements and plain text
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Accordion } from '../components';
+import OptForm from '../components/opt-form';
 import faqsData from '../fixtures/faqs.json';
 
 export function FaqsContainer() {
@@ -13,6 +14,14 @@ export function FaqsContainer() {
         </Accordion.Item>
       ))}
       <Accordion.Item />
+
+      <OptForm>
+        <OptForm.Input placeholder="Email" />
+        <OptForm.Button>COMENZÁ YA</OptForm.Button>
+        <OptForm.Text>
+          ¿Querés mirar Netflix ya? Ingresá tu email para crear una cuenta o reiniciar tu membresía de Netflix.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
